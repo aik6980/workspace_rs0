0da4d6ad47adcb0ec631684cb7cbe3b3eea7d091
aik6980 <aik6980>
2019-08-19 07:51:28 +0100

    update src

--- main_app/src/main.rs
+++ main_app/src/main.rs
@@ -1,20 +1,55 @@
-extern crate winit;
+// https://github.com/unknownue/vulkan-tutorial-rust/tree/master/src/tutorials
+
+use winit::{ ControlFlow, Event, EventsLoop, VirtualKeyCode, WindowEvent };
+
+// constants
+const WINDOW_TITLE : &'static str = "main_app";
+const WINDOW_DIM : (u32, u32) = (1024, 768);
+
+struct APP
+{
+    events_loop : EventsLoop,
+    window      : winit::Window,
+}
+
+impl APP
+{
+    pub fn new() -> APP
+    {
+        let events_loop = EventsLoop::new();
+        let window = APP::init_window(&events_loop);
+
+        APP
+        {
+            events_loop,
+            window,
+        }
+    }
+
+    fn init_window(events_loop : &EventsLoop) -> winit::Window
+    {
+        winit::WindowBuilder::new()
+            .with_title(WINDOW_TITLE)
+            .with_dimensions(WINDOW_DIM.into())
+            .build(&events_loop)
+            .expect("Failed to create window")
+    }
+
+    fn main_loop(&mut self)
+    {
+        self.events_loop.run_forever(|event|
+        {
+            match event 
+            {
+                Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => winit::ControlFlow::Break,
+                _ => winit::ControlFlow::Continue,
+            }
+        });
+    }
+}
 
 fn main()
 {
-    let mut events_loop = winit::EventsLoop::new();
-    let window = winit::Window::new(&events_loop).unwrap();
-
-    events_loop.run_forever(|event|
-    {
-        match event 
-        {
-            winit::Event::WindowEvent
-            {
-                event: winit::WindowEvent::CloseRequested,
-                ..
-            } => winit::ControlFlow::Break,
-            _ => winit::ControlFlow::Continue,
-        }
-    });
+    let mut app = APP::new();
+    app.main_loop();
 }
